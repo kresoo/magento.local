a60867d258b2d0dbf4a258a2e3ec3d16d1faaea1
Kresimir <kresimir.banovic@gmail.com>
2013-07-10 21:56:19 +0200

    test

--- app/code/community/Inchoo/Checkout/Model/Order.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Inchoo_Checkout_Model_Order extends Mage_Core_Model_Abstract
-{
-    protected function _construct() {
-        $this->_init('inchoo_checkout/order');
-    }
-}
--- app/code/community/Inchoo/Gift/Helper/Data.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class Inchoo_Gift_Helper_Data extends Mage_Core_Helper_Abstract
-{
-    
-}

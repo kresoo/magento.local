fc321f3deef479fc5da7e4c0a2b6bd833675a3cc
Kresimir Banovic <kresimir.banovic@inchoo.net>
2013-07-15 08:28:08 +0200

    changes

---
+++ app/code/community/Inchoo/Image/sql/inchoo_image_setup/install-1.0.0.0.php
@@ -0,0 +1,28 @@
+<?php
+
+$installer = $this;
+
+$installer->startSetup();
+
+$table = $installer->getConnection()
+        ->newTable($installer->getTable('inchoo_image/image'))
+        ->addColumn('image_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'identity' => true,
+            'primary' => true,
+            'nullable' => false
+        ),"Image ID")
+        ->addColumn('user_id', Varien_Db_Ddl_Table::TYPE_INTEGER, null, array(
+            'nullable' => false
+        ),"User ID")
+        ->addColumn('image_name', Varien_Db_Ddl_Table::TYPE_VARCHAR, 100, array(
+            'nullable' => false
+        ),"Image name")
+        ->addColumn('image_title', Varien_Db_Ddl_Table::TYPE_VARCHAR,100 , array(
+            'nullable' => false
+        ),"Image title");
+
+$installer->getConnection()->createTable($table);
+
+$installer->endSetup();
+
+?>
